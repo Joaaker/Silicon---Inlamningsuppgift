46a8c873810b3818e684a804e1b7a538cd4f19aa
Joakim <joakim.akerman@hotmail.com>
2024-09-19 15:00:06 +0200

    innan ny knapp

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
-// javascript skriven av ChatGPT
+// javascript skriven av Joakim Lindh och ChatGPT
+const darkmodeSwitch = document.getElementById('dark-mode');
+const hasDarkmode = localStorage.getItem('darkmode');
 const faqButtons = document.querySelectorAll('.dropdown-btn');
+
+
+if(hasDarkmode == null) {
+  if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enableDarkMode()
+  } else {
+    disableDarkMode()
+  }
+} else if(hasDarkmode === 'on') {
+  enableDarkMode()
+} else if(hasDarkmode === 'off') {
+  disableDarkMode()
+}
+
+
+
+darkmodeSwitch.addEventListener('change', () => {
+  if(darkmodeSwitch.checked) {
+    enableDarkMode()
+    localStorage.setItem('darkmode', 'on')
+  } else {
+    disableDarkMode()
+    localStorage.setItem('darkmode', 'off')
+  }
+})
+
+function enableDarkMode() {
+  darkmodeSwitch.checked = true
+  document.documentElement.classList.remove('light-mode');
+  document.documentElement.classList.add('dark-mode');
+}
+function disableDarkMode() {
+  darkmodeSwitch.checked = false
+  document.documentElement.classList.remove('dark-mode');
+  document.documentElement.classList.add('light-mode');
+}
+
 
 faqButtons.forEach(button => {
     button.addEventListener('click', () => {
         const answerId = button.getAttribute('aria-controls');
-        const answer = document.getElementById(answerId);
         const expanded = button.getAttribute('aria-expanded') === 'true';
 
         // Toggle the 'aria-expanded' attribute
@@ -15,3 +53,8 @@ faqButtons.forEach(button => {
         faqItem.classList.toggle('expanded', !expanded);
     });
 });
+
+
+
+
+
